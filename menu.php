88b07a637b0ef6b9af469b93b76c6ea9851b015f
ElmontaserMohammed <61362162+ElmontaserMohammed@users.noreply.github.com>
2020-03-07 23:39:28 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
 echo '<div class="clear"></div>
             <aside>
                 <ul>
-                    <li> <a href="../PHP/page1.php">Acceuil</a></li>
+                    <li> <a href="../PHP/page1.php">Acceuil</a></li>//retour a la premiere page
                 </ul>
             </aside>';
 ?>
